c69cfc5cb16bee728dcc9c570ace977adf50f055
Pushing the Greatness of Human Collaboration Forward <chad.wang.cn@gmail.com>
2014-12-13 17:29:51 +0800

    add periodcheck pool

--- engine/period_task_pool.py
+++ engine/period_task_pool.py
@@ -3,11 +3,42 @@ import sys
 import threading
 import  sched,time
 import traceback
+import datetime
 
+'''
+period task pool:
+
+'''
+
+class task_worker():
+        def __init__(self,_name,_func,_period):
+            self.name = _name
+            self.start_time = datetime.datetime.now() 
+            self.period = _period
+            self.func = _func
+            
+        def do_run(self):
+            try:
+                self.start_time = datetime.datetime.now() 
+                self.func()
+            except Exception,e:
+                print Exception,":",e
+                traceback.print_exc()
+        
+        def need_run(self):
+            diff = datetime.datetime.now() - self.start_time
+            if diff >= self.period:
+                return True
+            else:
+                return False 
+            
+            
 class period_task_pool(threading.Thread):
-    def __init__(self):
+    def __init__(self,_max_woker=2,_default_sleep=0):
         self.threadid= 0
         self.bExit =False
+        self.max_woker = _max_woker
+        self.default_sleep = _default_sleep
         threading.Thread.__init__(self)  
         self.SubcriberLst = {}
         self.rwlock = threading.RLock() #
@@ -33,7 +64,8 @@ class period_task_pool(threading.Thread):
             if self.SubcriberLst.has_key(_name):
                 ret = False
             else:
-                self.SubcriberLst[_name] = _func;
+                worker = task_worker.task_worker(_name,_func,_period)
+                self.SubcriberLst[_name] = worker;
                 ret = True
             self.rwlock.release()
         except Exception,e:
@@ -67,9 +99,16 @@ class period_task_pool(threading.Thread):
         try:
             self.rwlock.acquire()
             self.bExit = True
+            self.killAllWorker()
             self.Stop()
             self.rwlock.release()
         except Exception,e:
             print Exception,":",e
             traceback.print_exc() 
             self.rwlock.release() 
+    
+    def creatWorkerPool(self):
+        pass
+    
+    def killAllWorker(self):
+        pass
