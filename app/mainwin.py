e88b4879c3a9f55ca77707b563b6e2a2a288a00c
Pushing the Greatness of Human Collaboration Forward <chad.wang.cn@gmail.com>
2014-12-13 18:58:38 +0800

    delete comment code

--- app/mainwin.py
+++ app/mainwin.py
@@ -10,8 +10,7 @@ from PyQt4 import QtGui, QtCore, uic
 from PyQt4.QtCore import *
 
 if __name__ == '__main__':    
-    app=QApplication(sys.argv)  
-    #dialog = chart(800,600,0)
+    app=QApplication(sys.argv)      
     dialog=main_beta()  
     dialog.show()  
     app.exec_()  
